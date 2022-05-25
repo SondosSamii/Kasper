6f789961bc85addd406b776af0ca2a9b25b524bd
sondossamii <sondos.sami.abuzeid@gmail.com>
2022-05-26 01:49:33 +0200

    Add 'Portfolio' Section

--- index.js
+++ index.js
@@ -23,3 +23,24 @@ document.getElementById("toggle-menu").onclick = function () {
     navMenu.style.display = "none";
   }
 };
+
+function showContent(event) {
+  let thisTabClass = event.target.value,
+    allImgs = document.getElementById("tabsContent").children,
+    tabButtons = document.getElementsByClassName("tab-btn");
+  for (let i = 0; i < tabButtons.length; i++) {
+    tabButtons[i].classList.remove("active");
+    if (tabButtons[i].value === thisTabClass) {
+      tabButtons[i].classList.add("active");
+    }
+  }
+
+  for (let i = 0; i < allImgs.length; i++) {
+    const classFlag = allImgs[i].classList.contains(thisTabClass);
+    if (classFlag || thisTabClass === "all") {
+      allImgs[i].style.display = "block";
+    } else {
+      allImgs[i].style.display = "none";
+    }
+  }
+}
