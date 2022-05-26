0022b59aae3cfb97dc7900301bd3fc4ee9b588e5
sondossamii <sondos.sami.abuzeid@gmail.com>
2022-05-26 02:08:05 +0200

    Minimize images number to 8 only and Fix 'More' Button to show more than 8

--- index.js
+++ index.js
@@ -39,8 +39,23 @@ function showContent(event) {
     const classFlag = allImgs[i].classList.contains(thisTabClass);
     if (classFlag || thisTabClass === "all") {
       allImgs[i].style.display = "block";
+      // console.log(allImgs.length);
+    } else if (allImgs[i] > 7) {
+      allImgs[i].style.display = "none";
     } else {
       allImgs[i].style.display = "none";
     }
+    for (let j = 8; j < allImgs.length; j++) {
+      allImgs[j].style.display = "none";
+    }
+  }
+}
+
+function showMore() {
+  document.getElementById("showMore").style.display = "none";
+  let allImgs = document.getElementById("tabsContent").children;
+  for (let i = 8; i < allImgs.length; i++) {
+    console.log(allImgs[i]);
+    allImgs[i].style.display = "block";
   }
 }
